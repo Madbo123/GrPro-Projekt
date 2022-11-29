1ed8b7edef864238ba868fc6cda83f87d98cab9b
David <david.ringenson@gmail.com>
2022-11-29 17:07:39 +0100

    load media og singleton data container til media

--- VideoPlayerFX/src/main/java/com/GrPro/streamService/Controllers/Main.java
+++ VideoPlayerFX/src/main/java/com/GrPro/streamService/Controllers/Main.java
@@ -12,6 +12,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage startStage) throws Exception {
+        IOController.loadMedia();
         Parent root = FXMLLoader.load(getClass().getResource("LoginScreen.fxml"));
         startStage.initStyle(StageStyle.UNDECORATED);
         Scene scene = new Scene(root, 900, 600);
--- VideoPlayerFX/src/main/java/com/GrPro/streamService/Model/Serie.java
+++ VideoPlayerFX/src/main/java/com/GrPro/streamService/Model/Serie.java
@@ -6,8 +6,10 @@ public class Serie extends Media {
     int _endedYear;
     List<Integer> _season;
 
-    public Serie(String title, List<String> categories, double rating, int releaseYear) {
+    public Serie(String title, List<String> categories, double rating, int releaseYear,int _endedYear,List<Integer> _season) {
         super(title, categories, rating, releaseYear);
         typeOfMedia = "Serie";
+        this._endedYear = _endedYear;
+        this._season  = _season;
     }
 }
